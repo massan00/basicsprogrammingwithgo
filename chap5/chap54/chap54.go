5a2b38e2ff3dcb42b01ae0348da2e7144a4e6681
openista1 <planbplnacnow@gmail.com>
2022-06-21 09:26:42 +0900

    add chap54 kai_no_kosuu

--- chap5/chap54/chap54.go
+++ chap5/chap54/chap54.go
@@ -4,3 +4,15 @@ package chap54
 func Hanbetsushiki(a, b, c float64) float64 {
 	return (b * b) - (4 * a * c)
 }
+
+//2次方程式ax^2+bx+c=0の係数a,b,c(全て実数)を与えられたら解の個数を返す
+func Kai_no_kosuu(a, b, c float64) int {
+	num := Hanbetsushiki(a, b, c)
+	if num > 0 {
+		return 2
+	} else if num == 0 {
+		return 1
+	} else {
+		return 0
+	}
+}
--- chap5/chap54/chap54_test.go
+++ chap5/chap54/chap54_test.go
@@ -35,3 +35,42 @@ func TestHanbetsushiki(t *testing.T) {
 	}
 
 }
+
+func TestKainokosuu(t *testing.T) {
+
+	type args struct {
+		a float64
+		b float64
+		c float64
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "問題55 test1",
+			args: args{a: 2.0, b: 3.0, c: 4.0},
+			want: 0,
+		},
+		{
+			name: "問題55 test2",
+			args: args{a: 1.0, b: 5.0, c: 4.0},
+			want: 2,
+		},
+		{
+			name: "問題55 test3",
+			args: args{a: 1.0, b: 2.0, c: 4.0},
+			want: 0,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Kai_no_kosuu(tt.args.a, tt.args.b, tt.args.c); got != tt.want {
+				t.Errorf("Kai_no_kosuu() = %d, want= %d", got, tt.want)
+			}
+		})
+	}
+}
