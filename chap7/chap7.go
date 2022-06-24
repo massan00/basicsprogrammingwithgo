8cc63daabd1436922f1c1743e708b696158e827e
openista1 <planbplnacnow@gmail.com>
2022-06-24 11:48:25 +0900

    add up to question74

--- chap7/chap7.go
+++ chap7/chap7.go
@@ -1,5 +1,7 @@
 package chap7
 
+import "fmt"
+
 type SubjectScore struct {
 	nationalLanguage int
 	math             int
@@ -20,4 +22,42 @@ func GoukeiToHeikin(args SubjectScore) Score {
 	avarage := float64(total) / 5
 
 	return Score{total, avarage}
+}
+
+type Student struct {
+	name   string
+	result string
+}
+
+//名前と成績を受け取ったら文字列を返す
+func Seiseki(s Student) string {
+	return fmt.Sprintf("%sさんの評価は%sです。", s.name, s.result)
+}
+
+func (s *Student) Seiseki() string {
+	return fmt.Sprintf("%sさんの評価は%sです。", s.name, s.result)
+}
+
+//座標
+type Coordinate struct {
+	x float64
+	y float64
+}
+
+//x軸に対称な座標を返すメソッド
+func (c *Coordinate) SymmetricX() Coordinate {
+	c.y = c.y * -1
+	return *c
+}
+
+// 1つの座標を引数にとって中点の座標を返す
+func (c *Coordinate) Midpoint(c2 Coordinate) Coordinate {
+	if c.x == c2.x && c.y == c2.y {
+		return *c
+	}
+
+	x := (c.x + c2.x) / 2
+	y := (c.y + c2.y) / 2
+
+	return Coordinate{x: x, y: y}
 }
--- chap7/chap7_test.go
+++ chap7/chap7_test.go
@@ -40,3 +40,137 @@ func TestGoukeiToHeikin(t *testing.T) {
 		})
 	}
 }
+
+func TestSeiseki(t *testing.T) {
+	tests := []struct {
+		name string
+		args Student
+		want string
+	}{
+		{
+			name: "問題72 テスト1",
+			args: Student{name: "sato", result: "A"},
+			want: "satoさんの評価はAです。",
+		},
+		{
+			name: "問題72 テスト2",
+			args: Student{name: "tanaka", result: "B"},
+			want: "tanakaさんの評価はBです。",
+		},
+		{
+			name: "問題72 テスト3",
+			args: Student{name: "sasaki", result: "C"},
+			want: "sasakiさんの評価はCです。",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Seiseki(tt.args); got != tt.want {
+				t.Errorf("Seiseki()=%s, want = %s", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSeisekim(t *testing.T) {
+	tests := []struct {
+		name string
+		args Student
+		want string
+	}{
+		{
+			name: "メソッド 問題72 テスト1",
+			args: Student{name: "sato", result: "A"},
+			want: "satoさんの評価はAです。",
+		},
+		{
+			name: "メソッド 問題72 テスト2",
+			args: Student{name: "tanaka", result: "B"},
+			want: "tanakaさんの評価はBです。",
+		},
+		{
+			name: "メソッド 問題72 テスト3",
+			args: Student{name: "sasaki", result: "C"},
+			want: "sasakiさんの評価はCです。",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.args.Seiseki(); got != tt.want {
+				t.Errorf("Seiseki()=%s, want = %s", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSymmetricX(t *testing.T) {
+	tests := []struct {
+		name string
+		args Coordinate
+		want Coordinate
+	}{
+		{
+			name: "問題73 テスト1",
+			args: Coordinate{x: 1, y: 1},
+			want: Coordinate{x: 1, y: -1},
+		},
+		{
+			name: "問題73 テスト2",
+			args: Coordinate{x: 0, y: 0},
+			want: Coordinate{x: 0, y: 0},
+		},
+		{
+			name: "問題73 テスト3",
+			args: Coordinate{x: 10, y: -5},
+			want: Coordinate{x: 10, y: 5},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.args.SymmetricX(); got != tt.want {
+				t.Errorf("SymmetricX = %v, want = %v", got, tt.want)
+			}
+		})
+	}
+
+}
+
+func TestMidpoint(t *testing.T) {
+	tests := []struct {
+		name string
+		obj  Coordinate
+		args Coordinate
+		want Coordinate
+	}{
+		{
+			name: "問題74 テスト1",
+			obj:  Coordinate{x: 1, y: 1},
+			args: Coordinate{x: 1, y: 1},
+			want: Coordinate{x: 1, y: 1},
+		},
+		{
+			name: "問題74 テスト2",
+			obj:  Coordinate{x: 3, y: 3},
+			args: Coordinate{x: -3, y: -3},
+			want: Coordinate{x: 0, y: 0},
+		},
+		{
+			name: "問題74 テスト3",
+			obj:  Coordinate{x: 6, y: 4},
+			args: Coordinate{x: 2, y: 0},
+			want: Coordinate{x: 4, y: 2},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.obj.Midpoint(tt.args); got != tt.want {
+				t.Errorf("SymmetricX = %v, want = %v", got, tt.want)
+			}
+		})
+	}
+
+}
